61b0fb3e40ee092f25f63c8ef0718f9b70360e8a
Roy Hooper <rhooper@toybox.ca>
2017-10-01 20:36:38 -0400

    add management socket for stats gathering

--- files/run.sh
+++ files/run.sh
@@ -219,10 +219,11 @@ while [ ! -z "$conf" ]; do
     /bin/template.py -d /tmp/openvpn.$n.json -s /etc/openvpn/openvpn.conf.j2 -o /etc/openvpn/server-$n.conf
     echo "Started OpenVPN instance #$n"
     mkdir -p /var/run/openvpn
+    mkfifo /var/run/openvpn/server-0.sock
     rundaemon /usr/sbin/openvpn --status /var/run/openvpn/server-$n.status 10 \
                          --cd /etc/openvpn --script-security 2 --config /etc/openvpn/server-$n.conf \
-                         --writepid /var/run/openvpn/server-$n.pid \ 
-                         --management /var/run/var/run/server-$n.sock unix &
+                         --writepid /var/run/openvpn/server-$n.pid \
+                         --management /var/run/openvpn/server-$n.sock unix &
     n=$[ $n + 1 ]
     conf="$(get_openvpn_conf $n)"
 done
