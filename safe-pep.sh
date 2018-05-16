9b26fe21d4719e7b6cdcde6e84940523e30b78db
Leeno Jose <ljose@Leenos-MacBook-Pro.local>
2018-05-16 16:59:27 +0530

    tweaks on lists without IPs and cidr add/del options

--- safe-pep.sh
+++ safe-pep.sh
@@ -90,26 +90,52 @@ check_extenxion () {
 }
 
 do_ip () {
-    new_list="$1"
-    ipset list "$list_name" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort -u > work/tmp/$list_name.current.ips
-    local ips_to_import=`comm -13 work/tmp/$list_name.current.ips $new_list`
-
-    /sbin/ipset list | grep -q -w "$list_name"
-    if [ $? -eq 1 ]; then
-       /sbin/ipset -N "$list_name" iphash \
-	  || fail "Failed to create ipset $list_name"
-    fi
+    local new_list="$1"
+    local type="$2"
     
-    /sbin/iptables-save | grep -q -w "$list_name"
-    if [ $? -eq 1 ]; then
-       /sbin/iptables -I ENCRYPTME 2 -m set --match-set "$list_name" dst -j DROP \
-	  || fail "Failed to insert iptables rule $list_name"
+    if [ "$type" == "ip" ]; then
+       ipset list "$list_name" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' | sort -u > work/tmp/"$list_name".current.ips
+       local ips_to_import=`comm -13 work/tmp/$list_name.current.ips $new_list`
+
+       /sbin/ipset list | grep -q -w "$list_name"
+       if [ $? -eq 1 ]; then
+          /sbin/ipset -N "$list_name" iphash \
+	     || fail "Failed to create ipset $list_name"
+       fi
+    
+       /sbin/iptables-save | grep -q -w "$list_name"
+       if [ $? -eq 1 ]; then
+          /sbin/iptables -I ENCRYPTME 2 -m set --match-set "$list_name" dst -j DROP \
+	     || fail "Failed to insert iptables rule $list_name"
+       fi
+
+       for ip in $ips_to_import
+       do
+          /sbin/ipset -A "$list_name" "$ip"
+       done
     fi
 
-    for ip in $ips_to_import
-    do
-       /sbin/ipset -A "$list_name" "$ip"
-    done
+    if [ "$type" == "cidr" ]; then
+       ipset list "${list_name}_cidr" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,\}' | sort -u > work/tmp/"$list_name".current.cidr
+       local cidr_to_import=`comm -13 work/tmp/$list_name.current.cidr $new_list`
+
+       /sbin/ipset list | grep -q -w "${list_name}_cidr"
+       if [ $? -eq 1 ]; then
+          /sbin/ipset -N "${list_name}_cidr" hash:net \
+             || fail "Failed to create ipset ${list_name}_cidr"
+       fi
+
+       /sbin/iptables-save | grep -q -w "${list_name}_cidr"
+       if [ $? -eq 1 ]; then
+          /sbin/iptables -I ENCRYPTME 2 -m set --match-set "${list_name}_cidr" dst -j DROP \
+             || fail "Failed to insert iptables rule ${list_name}_cidr"
+       fi
+
+       for cidr in $cidr_to_import
+       do
+          /sbin/ipset -A "$list_name"_cidr "$cidr"
+       done
+    fi
 }
 
 reload_unboud () {
@@ -147,6 +173,17 @@ delete_list () {
     else
        echo "ipset "$list_name" not found"
     fi
+
+    /sbin/ipset list | grep -q -w "${list_name}"_cidr
+    if [ $? -eq 0 ]; then
+       /sbin/iptables-save | grep -q -w "${list_name}"_cidr
+       if [ $? -eq 0 ]; then
+          /sbin/iptables -D ENCRYPTME -m set --match-set "${list_name}"_cidr dst -j DROP
+       fi
+       /sbin/ipset destroy "${list_name}"_cidr
+    else
+       echo "ipset "${list_name}_cidr" not found"
+    fi
    
     docker exec -i encryptme bash -c "[ -f $unbound_list ]"
     if [ $? -eq 0 ]; then
@@ -164,17 +201,17 @@ destroy_list () {
        if [ "$list" == "whitelist" ]; then
 	  /sbin/iptables -D ENCRYPTME -m set --match-set "$list" dst -j ACCEPT \
              || fail "Failed to delete iptables rule for the list $list"
-       docker exec -i encryptme truncate -s 0 /usr/local/unbound-1.7/etc/unbound/whitelist.txt \
-          || fail "Failed to delete domain list $list.txt"
+          docker exec -i encryptme truncate -s 0 /usr/local/unbound-1.7/etc/unbound/whitelist.txt \
+             || fail "Failed to delete domain list $list.txt"
        else
           /sbin/iptables -D ENCRYPTME -m set --match-set "$list" dst -j DROP \
 	      || fail "Failed to delete iptables rule for the list $list"
        fi
        /sbin/ipset destroy "$list" \
           || fail "Failed to delete ipset $list"
-       docker exec -i encryptme rm -f /usr/local/unbound-1.7/etc/unbound/blacklists/"$list.txt" \
-          || fail "Failed to delete domain list $list.txt"
     done
+    docker exec -i encryptme rm -rf $blacklist_location \
+       || fail "Failed to delete domain lists"
     reload_unboud
 }
 
@@ -215,6 +252,7 @@ fi
 do_filter () {
     [ -d work/tmp ] || mkdir -p work/tmp
     local ip_file="$list_name.ips"
+    local cidr_file="$list_name.cidr"
     local domain_file="$list_name.domains"
     [ -z "$list_file" ] && {
        usage
@@ -224,13 +262,25 @@ do_filter () {
     if [ ! -f "$list_file" ]; then
        fail "File $list_file not found"
     else
-       grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $list_file \
+       grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' $list_file \
           | sort -u > work/tmp/"$ip_file"
-       grep -v '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $list_file \
+       grep -v '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' $list_file \
           | sort -u > work/tmp/"$domain_file"
+       grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\/[0-9]\{1,\}' $list_file \
+	  | sort -u > work/tmp/"$cidr_file"
+
     fi
-    do_ip work/tmp/"$ip_file"
-    do_domain work/tmp/"$domain_file"
+    if [ -s work/tmp/"$ip_file" ]; then 
+       do_ip work/tmp/"$ip_file" ip
+    fi
+
+    if [ -s work/tmp/"$domain_file" ]; then
+       do_domain work/tmp/"$domain_file"
+    fi
+
+    if [ -s work/tmp/"$cidr_file" ]; then
+       do_ip work/tmp/"$cidr_file" cidr
+    fi
 }
 
 check_for_list_name() {
@@ -258,7 +308,7 @@ check_for_list_name() {
        fail "No IP address or domain defined to whitelist"
     }
     
-    echo "$whitelist_me" | grep -q -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+    echo "$whitelist_me" | grep -q -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$'
     if [ $? -eq 0 ]; then
        whitelist_ip
     else
