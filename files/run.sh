7fd06254a01b500d484f639b7b8056d72dbd7132
Felipe Rodriguez <felipe.rodriguez@stackpath.com>
2019-07-03 17:17:28 -0500

    Iptables fix
    * Sed remove the comments
    * Sed remove blank lines left by removing the comments
    * Remove duplicate lines (duplicate iptables rules)
    * Restore the iprules with the clean up file

--- files/run.sh
+++ files/run.sh
@@ -319,17 +319,18 @@ done
     -o /etc/iptables.eme.rules \
     -v ipaddress=$DNS
 # TODO this leaves extra rules around
-# Always pull the rules that exists
+
+# Always pull the rules that are already in the host
 /sbin/iptables-save > /etc/iptables.original.rules
 # Merge the system rules and the eme rules
-cat /etc/iptables.original.rules >  /etc/iptables.rules
-cat /etc/iptables.eme.rules      >> /etc/iptables.rules
-# Restore the rules (might contain duplicate chains)
-/sbin/iptables-restore --noflush /etc/iptables.rules
-# Removing duplicated rules and restore iptables again
-# We need the iptables save format to properly remove duplicate rules 
-# We flush the chains after a proper parse
-/sbin/iptables-save | awk '/^COMMIT$/ { delete x; }; !x[$0]++' | /sbin/iptables-restore
+cat /etc/iptables.original.rules /etc/iptables.eme.rules > /etc/iptables.rules
+# Remove comments and blank lines (else the awk to remove dups will fail sometimes)
+sed  -i -e 's/^#.*$//g' -e '/^$/d' /etc/iptables.rules
+# Remove duplicate rules
+cat /etc/iptables.rules | awk '/^COMMIT$/ { delete x; }; !x[$0]++' > /etc/iptables.rules.clean
+# Restore the rules (cleaned up)
+/sbin/iptables-restore --noflush /etc/iptables.rules.clean
+
 
 
 
