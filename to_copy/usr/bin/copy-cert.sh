ecd0d1851bf715dde46a0d0146c5c1e2ad689286
Fernando Guerrero <fernando.guerrero@netprotect.com>
2019-11-05 11:39:51 -0600

    Updating openvpn conf

--- to_copy/usr/bin/copy-cert.sh
+++ to_copy/usr/bin/copy-cert.sh
@@ -5,15 +5,10 @@ INPUT="/tmp/tlsverify"
 OUTPUT="/etc/encryptme"
 
 
-fail() {
-    echo "${1-command failed}" >&2
-    exit ${2:-1}
-}
-
-
 echo "Copy $peer_cert from: $INPUT to $OUTPUT"
 
-cp "$INPUT/$peer_cert" "$OUTPUT" || fail "Couldn't copy file"
+
+cp "$INPUT/$peer_cert" "$OUTPUT" 
 
 
 exit 0
