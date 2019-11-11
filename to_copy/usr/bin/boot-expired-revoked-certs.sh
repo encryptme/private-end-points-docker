381c9cb1bb0123f123b0e8a21e32d97a2b16ba21
Fernando Guerrero <fernando.guerrero@netprotect.com>
2019-11-11 09:36:23 -0600

    Refactor openvpn scripts and rename them

---
+++ to_copy/usr/bin/boot-expired-revoked-certs.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -u
+
+LOCKFILE="/etc/encryptme/data/.cert_lock"
+SESSION_MAP=/etc/encryptme/data/cert_session_map
+URL_FILE=/etc/encryptme/pki/crl_urls.txt
+CRL_LIST_FILE=/tmp/crl.list
+
+
+fail() {
+    echo "${1-command failed}" >&2
+    [ -f $LOCKFILE ] && rm -f $LOCKFILE 
+    exit ${2:-1}
+}
+
+
+kill_session() {
+    local session="$1"
+
+    echo killing session: $session
+    sh /usr/bin/boot-cert.sh $session
+    [ $? -gt 0 ] && fail "Could not kill the session"
+
+    ## Remove it from the cert session
+    count=0
+    while [ -f "$LOCKFILE" ]; do
+        sleep 1
+        ((count++))
+        [ $count -gt 5 ] && fail "Could not get a lock"
+    done
+
+    echo "$$" > $LOCKFILE
+    grep -v $session $SESSION_MAP > /tmp/tmp_session_map
+    mv -f /tmp/tmp_session_map $SESSION_MAP
+    rm -f $LOCKFILE
+}
+
+
+terminate_expired_certs() {
+    cat $SESSION_MAP | while read line; do
+        end_date=$( echo $line | cut -d ',' -f 5 )
+        if [ -n "$end_date" ]; then
+            now_epoch=$( date +%s )
+            end_date_epoch=$( date -d "$end_date" +%s )
+            if [[ $now_epoch > $end_date_epoch ]]; then
+                session=$( echo $line | cut -d ',' -f 1 )
+                kill_session $session
+        fi  
+    done
+}
+
+
+terminate_revoked_certs() {
+    xargs -n 1 curl -s -o $CRL_LIST_FILE  < $URL_FILE
+
+    openssl crl -inform DER -text -noout -in $CRL_LIST_FILE  \
+        | grep "Serial Number" | sed 's/.*Serial Number: //g' \
+        | while read -r line ; do
+
+        # If it exists in the session file kill it
+        session=$(cat $SESSION_MAP | grep $line | awk '{split($0,a,","); print a[1]}')
+
+        if [ -n "$session" ]; then
+            kill_session $session
+        fi
+    done
+}
+
+
+terminate_expired_certs
+terminate_revoked_certs
+
--- to_copy/usr/bin/openvpn-on-connect.sh
+++ to_copy/usr/bin/openvpn-on-connect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -u
 
+
+# This script is run by openvpn server when a new client conection 
+# has been established, defined by "client-connect"
+
+
 LOCKFILE="/etc/encryptme/data/.cert_lock"
 SESSION_MAP="/etc/encryptme/data/cert_session_map"
 END_DATE_FILE="/etc/encryptme/data/cert_end_date"
@@ -20,9 +25,9 @@ while [ -f "$LOCKFILE" ]; do
     ##      start over.. maybe we lost something
     [ $count -gt 5 ] && fail "Could not get a lock"
 done
-
 echo "$$" > $LOCKFILE
 
+# Prune old records with the same common name
 grep -v $common_name $SESSION_MAP > tmp
 mv -f tmp $SESSION_MAP
 
@@ -30,8 +35,13 @@ serial_0=$(echo $tls_serial_hex_0 |tr -d : | tr '[:lower:]' '[:upper:]')
 serial_1=$(echo $tls_serial_hex_1 |tr -d : | tr '[:lower:]' '[:upper:]')
 serial_2=$(echo $tls_serial_hex_2 |tr -d : | tr '[:lower:]' '[:upper:]')
 
-end_date=$(grep $common_name $END_DATE_FILE | tail -1 | cut -d "," -f 2)
+end_date=$(grep $serial_0 $END_DATE_FILE | tail -1 | cut -d "," -f 3)
 
+# Stores client certificate info 
 echo $common_name,$serial_0,$serial_1,$serial_2,$end_date >> $SESSION_MAP
+
+grep -v $serial_0 $END_DATE_FILE > tmp
+mv -f tmp $END_DATE_FILE
+
 
 rm $LOCKFILE
