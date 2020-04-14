4f52127303eb9f3d0a1bba4bbb9ce59b5fff860c
Fernando Guerrero <fernando.guerrero@netprotect.com>
2020-04-14 17:43:45 -0500

    Identify the openvpn client who has disconnected to avoid sending its stats

--- to_copy/usr/bin/openvpn-client-disconnect.sh
+++ to_copy/usr/bin/openvpn-client-disconnect.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+CN_FILE="/tmp/common_names.txt"
+touch $CN_FILE
+
+# Stores the common_name from the client who has just disconnected
+# This value will be used by private-end-point-docker-stats
+# in order to avoid sending stats for this client
+echo $common_name >> $CN_FILE
+
 /usr/bin/send-metric.sh vpn_session
+
+# Prune common_name from file or erase the file
+grep -v $common_name $CN_FILE > tmp && \
+	mv -f tmp $CN_FILE || \
+	rm -f $CN_FILE
