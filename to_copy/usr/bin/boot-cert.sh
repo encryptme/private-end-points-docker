f6473ff1485436e969b83acde0d63d0500116062
Dmi Baranov <dmi.baranov@gmail.com>
2020-07-08 19:50:50 +0200

    Terminate sessions with expired or revoked certificates (#38)
    * Terminate users with expired or revoked certificates (#27)
    
    * Scripts to terminate users with exprired certs
    
    * Change PEP branch in Dockerfile
    
    * Fix typo
    
    * Fix openpvn config
    
    * General clean up
    
    * Copy client cert on connection
    
    * Updating openvpn conf
    
    * Restore openvpn conf
    
    * Updating openvpn config
    
    * Add tls-verify
    
    * Update path in copy-cert.sh
    
    * Fix copy-cert.sh
    
    * Append cert enddate to session_map
    
    * Refactor openvpn scripts and rename them
    
    * Restore Dockerfile and cron file
    
    * Add cron task to run 'boot-expired-revoked-certs.sh'
    
    * Add missing FI in 'terminate_expired_certs' function
    
    * Change cron permission
    
    * Store emails into the cert-session map
    
    * Fixes Typo
    
    * Fixes pattern to obtain email
    
    * Detect ipsec expired and revoked certs
    
    * Refactor code and avoid killing valid sessions
    
    * Minor fix
    
    * Adds variable quoting
    
    * Bump container's version to 0.11.2
    
    Co-authored-by: Fernando Guerrero <fernando.guerrero@netprotect.com>
    
    * Fix wrong date for expired certs (#37)
    
    Co-authored-by: Fernando Guerrero <fernando.guerrero@netprotect.com>
    
    Co-authored-by: fernando-guerrero-leon <fguerrero11+10@gmail.com>
    Co-authored-by: Fernando Guerrero <fernando.guerrero@netprotect.com>

---
+++ to_copy/usr/bin/boot-cert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -u
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH
+
+
+fail() {
+    echo "${1-command failed}" >&2
+    exit ${2:-1}
+}
+
+               
+for socket in "/var/run/openvpn/*.sock"; do
+    OPENVPN_CONNECTION=$(echo status | socat - UNIX-CONNECT:/var/run/openvpn/server-0.sock | grep "$1")
+    [ -n "$OPENVPN_CONNECTION" ] && break
+done
+
+IPSEC_CONNECTION=$(ipsec status | grep "$1" | sed -r 's/.*cloak\[([[:digit:]]+)\].+/\1/g')
+
+if [ -n "$IPSEC_CONNECTION" ]; then
+        echo "killing ipsec connection $1"
+        ipsec down cloak[$IPSEC_CONNECTION] || fail "Could not kill ipsec connection" "1"
+fi
+
+if [ -n "$OPENVPN_CONNECTION" ]; then
+        echo "Killing openvpn connection $1 ($OPENVPN_CONNECTION)"
+        echo "kill '$1'" | socat - UNIX-CONNECT:/var/run/openvpn/server-0.sock \
+        	|| fail "Could not kill openvpn connection" "1"
+fi
